67af8c52802563e36b176a4c506ecd0e30492c00
Sophia Shapira <Sophia.Shapira@gmail.com>
2026-04-06 21:46:35 -0400

    Made some improvements to the concept -- and renamed a function or two.

--- lib/Cartovanta.pm
+++ lib/Cartovanta.pm
@@ -9,13 +9,37 @@ use Exporter 'import';
 
 our @EXPORT_OK = qw(
   infile
-  rgval
-  onelzy
+  cr_eval_with_rg
+  cr_file_eval_with_rg
   shell_captr
   shell_qt
   shell_quote
   cr_resloc
+  cr_home_refresh
+  cr_settings
 );
+
+# DECLARE THE HELPER VARIABLES:
+my $_homedir;  # Home directory for the current user
+my $_refresh_home = 1; # Is the home auto-refreshed when applicable?
+
+
+
+# Get value for `$_homedir`
+cr_home_refresh();
+sub cr_home_refresh
+{
+  if ( defined($ENV{'HOME'}) && ($ENV{'HOME'} ne '') )
+  {
+    $_homedir = $ENV{'HOME'};
+  }
+  else
+  {
+    $_homedir = (getpwuid($<))[7];
+  }
+}
+
+
 
 # Loads the contents of a file into a PERL string.
 sub infile {
@@ -39,7 +63,7 @@ sub infile {
 # returned.
 # Otherwise, the return value is whatever is returned by the
 # generated code-reference.
-sub rgval {
+sub cr_eval_with_rg {
   my $lc_code;
   my $lc_bld;
   if ( !@_ ) { return undef; }
@@ -57,13 +81,13 @@ sub rgval {
 # return a code-reference, `undef` is returned.
 # Otherwise, the return value is whatever is returned by the
 # generated code-reference.
-sub onelzy {
+sub cr_file_eval_with_rg {
   my $lc_file;
   my $lc_code;
   if ( !@_ ) { return undef; }
   $lc_file = shift(@_);
   $lc_code = infile($lc_file);
-  return(rgval($lc_code,@_));
+  return(cr_eval_with_rg($lc_code,@_));
 }
 
 
@@ -96,7 +120,146 @@ sub shell_quote {
 }
 
 sub cr_resloc {
-  return(shell_captr('cartovanta','resloc',@_));
+  my $lc_found;
+  
+  # Refresh the home -- unless calling program has opted out
+  if ( $_refresh_home ) { cr_home_refresh(); }
+  
+  # At default, it is _not_ found.
+  $lc_found = undef;
+  
+  # First, let's try searching the custom search path.
+  if ( defined $ENV{'CARTOVANTA_RES_PATH'} )
+  {
+    my @lc2_path;
+    my $lc2_each;
+    @lc2_path = split(/:/,$ENV{'CARTOVANTA_RES_PATH'});
+    foreach $lc2_each (@lc2_path)
+    {
+      $lc_found = _check_res_dir($lc2_each,@_);
+      if ( defined($lc_found) ) { return $lc_found; }
+    }
+  }
+  
+  # Now we search default user-specific installs
+  $lc_found = _check_res_dir(($_homedir . '/local/cartovanta-res'),@_);
+  if ( defined($lc_found) ) { return $lc_found; }
+  
+  # Now we search for a default-loc systemwide install
+  return(_check_res_dir('/usr/local/cartovanta-res',@_));
+}
+
+sub _check_res_dir {
+  my $lc_chkdir; # The directory to check
+  my $lc_chktyp; # The type of directory
+  my $lc_chkres; # The resource to check for
+  my $lc_ret;    # Tentative return value
+  my $lc_each;
+  
+  # Make sure we have enough arguments.
+  if ( (scalar @_) < 2.5 )
+  {
+    return undef;
+  }
+  
+  # Load special variables from function arguments:
+  $lc_chkdir = shift(@_);
+  $lc_chktyp = shift(@_);
+  $lc_chkres = shift(@_);
+  
+  # Preliminary check:
+  $lc_ret = _check_preliminary($lc_chkdir,$lc_chktyp,$lc_chkres);
+  if ( !defined($lc_ret) ) { return undef; }
+  
+  # If we got this far, we do the stricter tests.
+  foreach $lc_each (@_)
+  {
+    if ( ref($lc_each) eq 'ARRAY' )
+    {
+      if ( _fail_extra_test($lc_ret,@$lc_each) ) { return undef; }
+    }
+  }
+  
+  # We made it!
+  return $lc_ret;
+}
+
+sub _check_preliminary {
+  my $lc_ret;
+  
+  # Reject all empty-string directories on path:
+  if ( $_[0] eq '' ) { return undef; }
+  
+  # Find the tentative result:
+  $lc_ret = ( $_[0] . '/' . $_[2] );
+  
+  # Check if resource exists if it is a file
+  if ( $_[1] eq 'f' )
+  {
+    if ( -f $lc_ret ) { return($lc_ret); }
+    return undef;
+  }
+  
+  # Check if resource exists if it is a directory
+  if ( $_[1] eq 'd' )
+  {
+    if ( -d $lc_ret ) { return($lc_ret); }
+    return undef;
+  }
+  
+  # Unidentified resource type -- fail:
+  return undef;
+}
+
+
+# This function is set to return 1 in case of a test
+# failing and 0 if it succeeds -- because that is
+# easier for the calling function to process.
+sub _fail_extra_test {
+  my $lc_lookfor;
+  
+  # Got we enough arguments?
+  if ( (scalar @_) < 2.5 ) { return 1; }
+  
+  # What must exist to pass?
+  $lc_lookfor = ( $_[0] . '/' . $_[2] );
+  
+  # File check
+  if ( $_[1] eq 'f' )
+  {
+    if ( -f $lc_lookfor ) { return 0; }
+    return 1;
+  }
+  
+  # Directory check
+  if ( $_[1] eq 'd' )
+  {
+    if ( -d $lc_lookfor ) { return 0; }
+    return 1;
+  }
+  
+  # Unidentified test must be a failure.
+  return 1;
+}
+
+# Let the calling program change settings for
+# this library.
+sub cr_settings {
+  
+  # The argument must be a hashref.
+  if ( ref($_[0]) ne 'HASH' ) { return 0; }
+  
+  # Auto-refreshing of home-directory (and possibly
+  # other things in the future) when applicable.
+  if ( defined($_[0]->{'refresh'}) )
+  {
+    if ( $_[0]->{'refresh'} )
+    {
+      $_refresh_home = 1;
+    } else {
+      $_refresh_home = 0;
+    }
+  }
 }
 
 
