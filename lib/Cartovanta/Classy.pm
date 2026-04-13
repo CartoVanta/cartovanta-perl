11dab94c71e23d76ad78da80b99dd36b5f4d3938
Sophia Shapira <Sophia.Shapira@gmail.com>
2026-04-12 22:28:43 -0400

    Created skeleton of `Cartovanta::Classy`

---
+++ lib/Cartovanta/Classy.pm
@@ -0,0 +1,14 @@
+# Copyright (c) 2026 - Sophia Elizabeth Shapira
+# This library is free software; you may redistribute it and/or modify
+# it under the same terms as Perl itself.
+
+package Cartovanta::Classy;
+
+use strict;
+use warnings;
+use Exporter 'import';
+
+our @EXPORT_OK = qw(
+);
+
+1;
