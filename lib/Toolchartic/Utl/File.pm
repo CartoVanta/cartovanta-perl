a4c80619756c57f319315af31202a6ba2b2c084b
Sophia Shapira <Sophia.Shapira@gmail.com>
2026-05-06 12:47:57 -0400

    `Toolchartic::Utl::File` is now a call-as-method package.

--- lib/Toolchartic/Utl/File.pm
+++ lib/Toolchartic/Utl/File.pm
@@ -3,22 +3,17 @@ package Toolchartic::Utl::File;
 # Usual package imports:
 use strict;
 use warnings;
-use Exporter 'import';
-
-
-our @EXPORT_OK = qw(
-  slurp_file
-  slurp_t_file
-);
-
 
 
 # Loads an entire file into one Perl string and returns it.
 # Returns undef if no filename was provided or if the file
 # could not be opened for reading.
 sub slurp_file {
+  my $this;
   my $lc_file;  # Pathname of the file to load
   my $lc_ret;   # File contents to be returned
+  
+  $this = shift(@_);
   
   if ( (scalar @_) < 0.5 ) { return undef; }
   
@@ -38,7 +33,10 @@ sub slurp_file {
 # Slurps in a text file and assures that the text gets
 # normalized to Unix-style.
 sub slurp_t_file {
+  my $this;
   my $lc_text;
+  
+  $this = shift(@_);
   
   $lc_text = slurp_file($_[0]);
   if ( !(defined($lc_text)) ) { return undef; }
